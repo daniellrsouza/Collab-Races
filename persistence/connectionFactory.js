abb14e308f56a016e93eb0da497793749e74aa47
Daniel Souza <daniellrs3@hotmail.com>
2018-04-30 03:01:37 -0300

    updating the project

--- persistence/connectionFactory.js
+++ persistence/connectionFactory.js
@@ -1,9 +1,8 @@
 var pg = require('pg');
 var connection = process.env.DATABASE_URL;
-const client = new pg.Client(connection);
 
 function createDBConnection() {
-    return client.connect();
+    return new pg.Client(connection);
 }
 
 module.exports = function() {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     app.get('/motoristas', function(req, res) {
-        console.log('Recebida requisição de teste na porta 3000.')
+        
         var connection = app.persistence.connectionFactory();
         var motoristaDAO = new app.persistence.MotoristaDAO(connection);
 
