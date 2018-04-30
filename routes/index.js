be8b03a8ccc5f9511192715998487401a7fbb055
Daniel Souza <daniellrs3@hotmail.com>
2018-04-30 02:40:42 -0300

    updating the project

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,9 @@ module.exports = function(app) {
         var connection = app.persistence.connectionFactory();
         var motoristaDAO = new app.persistence.MotoristaDAO(connection);
 
-        motoristaDAO.lista(erro, function(resultado) {
-            if(erro) {
-                console.log('erro ao inserir no banco:'+erro);
-                res.status(400).send(erro);
-            } else {
-                console.log(resultado);
-                res.json(resultado);
-            }
+        motoristaDAO.lista(function(resultado) {
+            console.log(resultado);
+            res.json(resultado);
         });
     });
 
